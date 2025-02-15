46f97aa73980847590b75dd0f88271a5941d0913
Jay <Jay@geekstudio.co.kr>
2025-02-15 19:39:06 +0900

    :hammer: Add: db 커넥션 정보 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as process from 'process';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    AuthModule,
     ConfigModule.forRoot({
       envFilePath: `.env.${process.env.NODE_ENV || 'dev'}`, // 환경에 맞는 .env 파일 로드
       isGlobal: true, // env 전역에서 사용 가능하게 설정
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_DEFAULT_HOST'),
+        port: configService.get<number>('DB_DEFAULT_PORT'),
+        username: configService.get<string>('DB_DEFAULT_USER'),
+        password: configService.get<string>('DB_DEFAULT_PASSWORD'),
+        database: configService.get<string>('DB_DEFAULT_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: false,
+        charset: 'utf8mb4_unicode_ci',
+      }),
+    }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
