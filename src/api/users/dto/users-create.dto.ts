bdd1609de60ec963730651c6fe421dea009d695b
aalon <aaron@geekstudio.co.kr>
2025-02-17 11:34:53 +0900

    Fix: gs_users column name 변경

--- src/api/users/dto/users-create.dto.ts
+++ src/api/users/dto/users-create.dto.ts
@@ -1,5 +1,5 @@
 import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
-import { MANAGER_TYPE } from '../../../common/enum/enum';
+import { USER_TYPE } from '../../../common/enum/enum';
 
 export class UsersCreateDto {
   @IsString()
