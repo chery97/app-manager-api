b99e94fd3a93f3e4b08ca2997f2b108af1a7f8df
Jay <jay@geekstudio.co.kr>
2025-02-17 14:01:47 +0900

    :wrench: Add: firebase.config 파일 추가

---
+++ src/common/config/fcm/firebase.config.ts
@@ -0,0 +1,22 @@
+import * as admin from 'firebase-admin';
+import { ServiceAccount } from 'firebase-admin';
+import sbgolfAccount from './json/firebase-service-account-sbgolf-test.json';
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+// [Jay] 고객사별 Firebase 프로젝트 등록
+const firebaseApps = {
+  sbgolfTest: admin.initializeApp(
+    {
+      credential: admin.credential.cert(sbgolfAccount as ServiceAccount),
+    },
+    'sbgolfTest',
+  ),
+};
+
+// 특정 앱의 Firebase 인스턴스 반환
+export const getFirebaseApp = (appName: string): admin.app.App => {
+  if (!firebaseApps[appName]) {
+    throw new HttpException('해당하는 앱이 없습니다.', HttpStatus.BAD_REQUEST);
+  }
+  return firebaseApps[appName];
+};
