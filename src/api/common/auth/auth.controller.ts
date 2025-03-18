0f0d8bb597310e7e1d6aad528c31cb59f3f431b1
aalon <aaron@geekstudio.co.kr>
2025-03-18 15:49:02 +0900

    feat: token 검증 api 추가

--- src/api/common/auth/auth.controller.ts
+++ src/api/common/auth/auth.controller.ts
@@ -1,13 +1,43 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import {
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+  Req,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('common/auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   @Post('refresh')
-  async getMyRefreshToken(@Req() req: any) {
+  async refreshToken(@Req() req: any) {
     const refreshToken = req.body?.refreshToken;
-    return await this.authService.getMyRefreshToken(refreshToken);
+    return await this.authService.refreshToken(refreshToken);
+  }
+
+  @Post('token-check')
+  async verify(@Req() req: any) {
+    try {
+      const bearerToken = req.headers['authorization']
+        .toString()
+        .replace('Bearer', '')
+        .trim();
+      return this.jwtService.verify(bearerToken + 'a', {
+        secret: process.env.ACCESS_SECRET_KEY,
+      });
+    } catch (error) {
+      const message = {
+        'invalid token': '유효하지 않은 토큰입니다.',
+        'jwt expired': '토큰값이 만료되었습니다.',
+        'jwt malformed': '잘못된 형식의 토큰입니다.',
+      };
+      throw new HttpException(message[error.message], HttpStatus.UNAUTHORIZED);
+    }
   }
 }
